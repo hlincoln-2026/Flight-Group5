b146c7c6da5df8817ef0b69ea331bf412c3501c7
Hunter Lincoln <144962317+hlincoln-2026@users.noreply.github.com>
2025-02-15 20:01:21 +0100

    Added 3 new function
    Added euclidean_distance which calculates euclidean distance. Added calc_distances which calculates distances for each airport in a given list. Added create_histogram which uses euclidean distances to create a vistual diagram of the data

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 def main():
     df = pd.read_csv('airports.csv')
-    part1.all_airports(df)
-
+    #part1.all_airports(df)
+    lst = ['GRR','LAX','AAF','ABR']
+    part1.create_histogram(df,lst)
 
 if __name__ == '__main__':
     main()
