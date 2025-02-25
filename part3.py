e9d96e82ec287299cad2a26594d2f03f1b85795a
Hunter Lincoln <144962317+hlincoln-2026@users.noreply.github.com>
2025-02-25 21:33:00 +0100

    Worked on more functions
    Worked on average_departure_delay, delayed_flights_by_destination, and top_airplane_manufacturers

--- part3.py
+++ part3.py
@@ -1,6 +1,7 @@
 import sqlite3
 import part1
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 def verify_computed_distances():
@@ -32,6 +33,12 @@ def get_flight_statistics():
     """Return statistics such as flight count, unique destinations, and most frequent destination."""
     pass
 
+
+'''
+Parameters:
+    departure -> faa code for airport the flight is departing from
+    arrival -> faa code for the airport the flight will be arriving at
+'''
 def get_airplane_usage(departure, arrival):
     """Return a dictionary describing the number of times each plane type was used for a specific route."""
 
@@ -69,15 +76,123 @@ def get_airplane_usage(departure, arrival):
 
 def average_departure_delay():
     """Compute and visualize the average departure delay per airline."""
-    pass
 
-def delayed_flights_by_destination():
+    delay_dict = {} #Empty dictionary to hold delay times for each airline
+
+    conn = sqlite3.connect('flights_database.db')
+    cursor = conn.cursor()
+
+    query1 = f'SELECT * FROM airlines'
+    query2 = f'SELECT dep_delay, carrier FROM flights'
+
+    cursor.execute(query1)
+    airlines_df = pd.DataFrame(cursor.fetchall(), columns = [x[0] for x in cursor.description]) 
+    airlines_df.index = airlines_df['carrier'] #Sets airline dataframe indexes to be their carrier
+
+    cursor.execute(query2)
+    flights_df = pd.DataFrame(cursor.fetchall(), columns = [x[0] for x in cursor.description])
+
+    airline_set = set(airlines_df['carrier']) #Creates set of airline carriers, getting all the names with no duplicants
+
+    # Iterates through each carrier
+    for carrier in airline_set:
+        filtered_flights = flights_df[flights_df['carrier'] == carrier] #Filteres flight dataframe by that carrier
+
+        sum = filtered_flights['dep_delay'].sum() #Gets the sum of all the delays for that carrier
+        total = filtered_flights['dep_delay'].size #Gets the total number of flights for that carrier
+
+        average = round(sum / total, 2) #Calculates the average for each carrier and rounds it to 2 decimal places
+
+        name = airlines_df.loc[carrier] #Gets the full airline name by the carrier index
+        delay_dict[name[1]] = average #Adds the carrier to the dictionary and sets its value to the average delay time
+
+    #Sets up pyplot
+    plt.figure(figsize=(7,7))
+    plt.bar(delay_dict.keys(), delay_dict.values()) #Sets the x-axis as the keys of the dictonary(carrier names) and y-axis as the values(averages)
+    plt.xlabel('Airline Carriers')
+    plt.ylabel('Average Delay Time')
+    plt.title('Delay Times for Airlines')
+    plt.xticks(rotation='vertical')
+    plt.subplots_adjust(bottom = 0.3) 
+    plt.show()
+
+    
+'''
+Parameters:
+    months -> range of months as a list of integers ([January, Febuary] --> [1,2])
+    dest -> faa code for the destination airport
+'''
+def delayed_flights_by_destination(months, dest):
     """Return the number of delayed flights to a given destination within a specified time range."""
-    pass
+    
+    conn = sqlite3.connect('flights_database.db')
+    cursor = conn.cursor()
 
-def top_airplane_manufacturers():
+    query1 = f'SELECT month,dep_delay,dest FROM flights'
+
+    cursor.execute(query1)
+
+    flights_df = pd.DataFrame(cursor.fetchall(), columns = [x[0] for x in cursor.description]) #Creates dataframe from flights table
+    
+    # Filters the flights dataframe by destination airport and if the flight is in the given range of months
+    flights_df = flights_df[flights_df['dest'] == dest]
+    flights_df = flights_df[flights_df['month'].isin(months)]
+
+    delays = list(flights_df['dep_delay']) #Creates a list from departure delay column of dataframe
+    
+    delayed_flights = 0 #Counter for number of delayed flights
+    # Iterates through each delay value
+    for num in delays:
+        #If the value is positive that means a delay occurred and the delayed flight counter will be increased by 1
+        if num > 0:
+            delayed_flights += 1
+
+    return delayed_flights # Returns the total number of delayed flights
+
+'''
+Parameters:
+    dest -> faa code for destination airport
+'''
+def top_airplane_manufacturers(dest):
     """Return the top 5 airplane manufacturers with planes departing to the specified destination."""
-    pass
+    
+    lst = []
+
+    conn = sqlite3.connect('flights_database.db')
+    cursor = conn.cursor()
+
+    query1 = f'SELECT tailnum, dest FROM flights'
+    cursor.execute(query1)
+    flights_df = pd.DataFrame(cursor.fetchall(), columns = [x[0] for x in cursor.description]) #Dataframe of flights data
+    
+    query2 = f'SELECT tailnum, manufacturer FROM planes'
+    cursor.execute(query2)
+    planes_df = pd.DataFrame(cursor.fetchall(), columns = [x[0] for x in cursor.description]) #Dataframe of planes data
+
+    flights_df = flights_df[flights_df['dest'] == dest] #Fliters the flights dataframe based on the given desitnation airport
+    
+    tailnum_lst = list(flights_df['tailnum']) #Creates a list of all the tailnum values in the filtered flights dataframe
+
+    manufacturers = set(planes_df['manufacturer']) #Gets all the names of the manufacturers in the planes dataframe
+
+    # Iterates throught each manufacture
+    for man in manufacturers:
+        #Filters the plane data frame based on the the current manufacturer and if the tailnum is from a flight to given destination
+        df = planes_df[planes_df['manufacturer'] == man]
+        df = df[df['tailnum'].isin(tailnum_lst)]
+
+        lst.append((df.size,man)) # Adds a tuple to a list in the form (number of planes creates, manufacturer)
+
+    lst.sort(reverse=True,key=lambda tup: tup[0]) #Sorts list from high to low based on number of planes created by the manufacturer
+    
+    #Creates a list of the top five manufacturer names
+    rtn_lst = []
+    for i in range(5):
+        rtn_lst.append(lst[i][1])
+    
+    return rtn_lst #Returns the list of top 5 manufacturers
+
+    
 
 def analyze_distance_vs_arrival_delay():
     """Investigate the relationship between flight distance and arrival delay time."""
